98c987755e088ae304e74e050c80fbc32db9db63
vasu <vasusel@gmail.com>
2024-02-17 12:56:59 +0530

    dev1-add xyz calss

--- GitProj01/src/com/nt/dao/Abc.java
+++ GitProj01/src/com/nt/dao/Abc.java
@@ -4,6 +4,7 @@ public class Abc {
 
 	public static void main(String[] args) {
 		System.out.println("MAIN Method is excecuted::");
+		
 
 	}
 
---
+++ GitProj01/src/com/nt/dao/xyz.java
@@ -0,0 +1,9 @@
+package com.nt.dao;
+
+public class xyz {
+
+	public static void main(String[] args) {
+	    System.out.println("xyz.main()");
+	}
+
+}
